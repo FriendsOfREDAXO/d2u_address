21ab8d15b575f1b9cc50dc45fc39d768fd60d810
tobias <tobias@UbuntuXPS>
2019-01-21 23:59:35 +0100

    hide language fields if not to be saved

--- pages/setup.php
+++ pages/setup.php
@@ -27,6 +27,7 @@ $d2u_module_manager->showManagerList();
 <h2>Changelog</h2>
 <p>1.1.1-DEV:</p>
 <ul>
+	<li>Sprachdetails werden ausgeblendet, wenn Speicherung der Sprache nicht vorgesehen ist.</li>
 	<li>Bugfix: Es wurden nur positive Werte bei den Geokoordinaten gespeichert.</li>
 </ul>
 <p>1.1.0:</p>
