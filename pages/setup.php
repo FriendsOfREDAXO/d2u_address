a70969c18d540aa09d5107f775c1fb893ece33b1
tobias <tobias@UbuntuXPS>
2022-06-03 15:56:03 +0200

    finalize next release

--- pages/setup.php
+++ pages/setup.php
@@ -25,7 +25,7 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_address" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
-<p>1.2.1-DEV:</p>
+<p>1.2.1:</p>
 <ul>
 	<li>Anpassungen an Publish Github Release to Redaxo.</li>
 	<li>Sortiung der Adressen bei allen Methoden vereinheitlicht: Sortierung erfolgt ausschließlich nach Priorität.</li>
