13ffc0facb3c60a1c91848bf88b5d41750057719
Tobias Krais <tk@design-to-use.de>
2023-06-12 13:15:50 +0200

    prepare next version

--- pages/setup.php
+++ pages/setup.php
@@ -25,12 +25,16 @@ $d2u_module_manager->showManagerList();
 <h2>Support</h2>
 <p>Fehlermeldungen bitte im <a href="https://github.com/TobiasKrais/d2u_address" target="_blank">GitHub Repository</a> melden.</p>
 <h2>Changelog</h2>
+<p>1.4.1-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.4.0:</p>
 <ul>
 	<li>PHP-CS-Fixer Verbesserungen.</li>
 	<li>rexstan Abgleich.</li>
 	<li>Modul 20-1 "D2U Adressen - Adressausgabe": Kann nun auch Karten des Geolocation Addons Verion 2 verwenden und zeigt in den Infoboxen der Karten nun auch Telefonnummern an.</li>
-	<li>Modul 20-3 "D2U Adressen - Weltkarte" hinzugefügt. Es werden alle Kontakte einer Kontaktart angezeigt.
+	<li>Modul 20-3 "D2U Adressen - Weltkarte" hinzugefügt. Es werden alle Kontakte einer Kontaktart angezeigt.</li>
 </ul>
 <p>1.3.0:</p>
 <ul>
